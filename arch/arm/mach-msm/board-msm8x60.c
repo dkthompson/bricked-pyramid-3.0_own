8345125229af8f4312c0020069568d4684635491
showp1984 <showp1984@gmail.com>
2012-01-08 01:53:36 +0100

    Initial pyramid board support

--- arch/arm/mach-msm/board-msm8x60.c
+++ arch/arm/mach-msm/board-msm8x60.c
@@ -9948,6 +9948,10 @@ static struct msm_board_data msm8x60_dragon_board_data __initdata = {
 	.gpiomux_cfgs = msm8x60_dragon_gpiomux_cfgs,
 };
 
+static struct msm_board_data msm8x60_pyramid_board_data __initdata = {
+	.gpiomux_cfgs = msm8x60_pyramid_gpiomux_cfgs,
+};
+
 static void __init msm8x60_init(struct msm_board_data *board_data)
 {
 	uint32_t soc_platform_version;
@@ -10275,6 +10279,11 @@ static void __init msm8x60_dragon_init(void)
 	msm8x60_init(&msm8x60_dragon_board_data);
 }
 
+static void __init msm8x60_pyramid_init(void)
+{
+	msm8x60_init(&msm8x60_pyramid_board_data);
+}
+
 MACHINE_START(MSM8X60_RUMI3, "QCT MSM8X60 RUMI3")
 	.map_io = msm8x60_map_io,
 	.reserve = msm8x60_reserve,
@@ -10351,6 +10360,16 @@ MACHINE_START(MSM8X60_DRAGON, "QCT MSM8X60 DRAGON")
 	.init_irq = msm8x60_init_irq,
 	.handle_irq = gic_handle_irq,
 	.init_machine = msm8x60_dragon_init,
+	.timer = &msm_timer,
+	.init_early = msm8x60_charm_init_early,
+MACHINE_END
+
+MACHINE_START(MSM8X60_PYRAMID, "QCT MSM8X60 PYRAMID")
+	.map_io = msm8x60_map_io,
+	.reserve = msm8x60_reserve,
+	.init_irq = msm8x60_init_irq,
+	.handle_irq = gic_handle_irq,
+	.init_machine = msm8x60_pyramid_init,
 	.timer = &msm_timer,
 	.init_early = msm8x60_charm_init_early,
 MACHINE_END
